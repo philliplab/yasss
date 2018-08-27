f8c62950baf7c0a2a141d2e6452d3a16239ac0ae
Phillip Labuschagne <philliplab@gmail.com>
2018-08-27 18:42:55 +0200

    Add tests for sim_pop argument checks

--- tests/testthat/test_sim_pop.R
+++ tests/testthat/test_sim_pop.R
@@ -12,4 +12,16 @@ test_that("sim_pop checks arguments correctly", {
                N_GEN_N_POP_INF_NULL)
   expect_error(sim_pop(ancestors = c('AAA'), n_gen = Inf, n_pop = Inf),
                N_GEN_N_POP_INF_NULL)
+
+  N_GEN_N_POP_LESS_ONE <- "Neither n_gen nor n_pop may be set to less than one"
+  expect_error(sim_pop(ancestors = c('AAA'), n_gen = 0),
+               N_GEN_N_POP_LESS_ONE)
+  expect_error(sim_pop(ancestors = c('AAA'), n_gen = -5),
+               N_GEN_N_POP_LESS_ONE)
+  expect_error(sim_pop(ancestors = c('AAA'), n_pop = 0.4),
+               N_GEN_N_POP_LESS_ONE)
+  expect_error(sim_pop(ancestors = c('AAA'), n_pop = -100),
+               N_GEN_N_POP_LESS_ONE)
+  expect_error(sim_pop(ancestors = c('AAA'), n_gen = 0, n_pop = 0),
+               N_GEN_N_POP_LESS_ONE)
 })
