c851e2e7292c1b476475828c0c8c433aba098101
Phillip Labuschagne <philliplab@gmail.com>
2018-08-27 18:38:28 +0200

    tests for sim_pop argument checking

--- tests/testthat/test_sim_pop.R
+++ tests/testthat/test_sim_pop.R
@@ -2,6 +2,14 @@ context('sim_pop')
 
 library(yasss)
 
-test_that("sim_pop produces output of correct length", {
-  expect_equal(1+1, 2)
+test_that("sim_pop checks arguments correctly", {
+  N_GEN_N_POP_INF_NULL <- "Either n_gen or n_pop must be specified and at least one must be finite"
+  expect_error(sim_pop(ancestors = c('AAA')), 
+               N_GEN_N_POP_INF_NULL)
+  expect_error(sim_pop(ancestors = c('AAA'), n_gen = Inf),
+               N_GEN_N_POP_INF_NULL)
+  expect_error(sim_pop(ancestors = c('AAA'), n_pop = Inf),
+               N_GEN_N_POP_INF_NULL)
+  expect_error(sim_pop(ancestors = c('AAA'), n_gen = Inf, n_pop = Inf),
+               N_GEN_N_POP_INF_NULL)
 })
