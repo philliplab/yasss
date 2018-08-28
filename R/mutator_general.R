3c490d58527f435ec6cc6343c60844e08f171ca0
Phillip Labuschagne <philliplab@gmail.com>
2018-08-28 20:18:46 +0200

    ADD extra checks to general mutator checker

--- R/mutator_general.R
+++ R/mutator_general.R
@@ -15,16 +15,29 @@ mutator_checks_general <- function(fun, args){
   fun_is_fun <- class(fun) == "function"
   fun_has_parent_arg <- "parent" %in% names(formals(fun))
   fun_has_mu_arg <- "mu" %in% names(formals(fun))
-  
+
+  all_mu_greater_equal_0 <- all(args$mu >= 0)
+  all_mu_less_equal_1 <- all(args$mu <= 1)
+
   parent <- 'ACGTAC'
   tmp_args <- c(list(parent = parent), args)
   x <- do.call(fun, tmp_args)
 
   output_is_list <- class(x) == 'list'
+  output_has_parent <- 'parent' %in% names(x)
+  output_has_child <- 'child' %in% names(x)
+  output_has_mutation_stats <- 'mutation_stats' %in% names(x)
+  output_has_mu <- 'mu' %in% names(x)
 
-  results <- list(fun_is_fun = fun_is_fun,
-                  fun_has_parent_arg = fun_has_parent_arg,
-                  fun_has_mu_arg = fun_has_mu_arg,
-                  output_is_list = output_is_list)
-  return(results)
+  #NOTE: Might have to update this to handle indels
+  # Because might want to use this to track insertion positions
+  output_parent_correct <- parent == x$parent
+  output_child_char <- class(x$child) == 'character'
+  output_one_child <- length(x$child) == 1
+
+  result <- list(fun_is_fun = fun_is_fun,
+                 fun_has_parent_arg = fun_has_parent_arg,
+                 fun_has_mu_arg = fun_has_mu_arg,
+                 output_is_list = output_is_list)
+  return(result)
 }
