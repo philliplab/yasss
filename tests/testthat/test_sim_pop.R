ac6d162049ec802213a5c690383d102e933884c0
Phillip Labuschagne <philliplab@gmail.com>
2018-08-28 12:55:54 +0200

    add tests for n_gen argument

--- tests/testthat/test_sim_pop.R
+++ tests/testthat/test_sim_pop.R
@@ -32,3 +32,19 @@ test_that("sim_pop output in the correct format", {
   expect_equal(class(x), 'list')
   expect_true("seqs" %in% names(x))
 })
+
+test_that("sim_pop produces the correct number of sequences", {
+  x <- sim_pop(ancestors = c("AAAA", "BBBB"),
+               n_gen = 1)
+  expect_equal(length(x$seqs), 4)
+
+  x <- sim_pop(ancestors = c("AAAA", "BBBB"),
+               n_gen = 2)
+  expect_equal(length(x$seqs), 8)
+
+  x <- sim_pop(ancestors = c("AAAA", "BBBB"),
+               n_gen = 3)
+  expect_equal(length(x$seqs), 16)
+
+})
+
