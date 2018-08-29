cf60f680b2b1fc2e2d0d1fb86bb8877f4d98182a
Phillip Labuschagne <philliplab@gmail.com>
2018-08-29 10:16:06 +0200

    ADD tests to mutator_uniform expecting reasonable number of mutations for mu=0.05

--- tests/testthat/test_mutator_uniform.R
+++ tests/testthat/test_mutator_uniform.R
@@ -7,8 +7,8 @@ context("uniform_mutator")
 #                      )
 
 test_that("uniform_mutator introduces zero mutations on mu=0", {
-  fun = mutator_uniform_fun
-  args = list(mu = 0)
+  fun <- mutator_uniform_fun
+  args <- list(mu = 0)
   parents <- c('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
                'ACGTACGTA',
                paste(rep('AAACCCGGGTTT', 10), sep = '', collapse = ''),
@@ -20,4 +20,16 @@ test_that("uniform_mutator introduces zero mutations on mu=0", {
     expect_equal(x$parent, x$child, info = parent)
     expect_equal(x$mutation_stats$n_mut, 0, info = parent)
   }
+
+  args <- list(mu = 0.05)
+  args$parent <- paste(rep('AAACCCGGGTTT', 1000), sep = '', collapse = '')
+  max_mut <- 750
+  min_mut <- 450
+  
+  for (i in 1:10){
+    x <- do.call(fun, args)
+    expect_lt(x$mutation_stats$n_mut, max_mut)
+    expect_gt(x$mutation_stats$n_mut, min_mut)
+  }
 })
+
