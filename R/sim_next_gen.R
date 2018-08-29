bb0d36af90247718dec248bdd08d12f3ecce8bc0
Phillip Labuschagne <philliplab@gmail.com>
2018-08-29 12:54:26 +0200

    ADD looping structure into sim_next_gen

--- R/sim_next_gen.R
+++ R/sim_next_gen.R
@@ -7,5 +7,14 @@
 #' @export
 
 sim_next_gen <- function(parents, gen_size){
-  return (rep(parents, gen_size))
+  offspring <- character(length(parents)*gen_size)
+  total_offspring <- 0
+  for (i in 1:length(parents)){
+    for (j in 1:gen_size){
+      total_offspring <- total_offspring + 1
+      child <- parents[i]
+      offspring[total_offspring] <- child
+    }
+  }
+  return(offspring)
 }
