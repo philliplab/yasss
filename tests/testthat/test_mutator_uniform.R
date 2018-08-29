7209622b105e4320b648eb544f93e90fbc418227
Phillip Labuschagne <philliplab@gmail.com>
2018-08-29 10:08:27 +0200

    ADD tests for mutator_uniform mu=0 leads to no mutations

--- tests/testthat/test_mutator_uniform.R
+++ tests/testthat/test_mutator_uniform.R
@@ -1,5 +1,23 @@
 context("uniform_mutator")
 
-test_that("uniform_mutator works", {
-  expect_equal(1+1,2)
+#YASSS_MUTATORS <- list(mutator_uniform = 
+#                         list(fun = mutator_uniform_fun,
+#                              args = list(mu = 0.01),
+#                              checker = c)
+#                      )
+
+test_that("uniform_mutator introduces zero mutations on mu=0", {
+  fun = mutator_uniform_fun
+  args = list(mu = 0)
+  parents <- c('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+               'ACGTACGTA',
+               paste(rep('AAACCCGGGTTT', 10), sep = '', collapse = ''),
+               paste(rep('AAACCCGGGTTT', 1000), sep = '', collapse = ''))
+
+  for (parent in parents){
+    args$parent <- parent
+    x <- do.call(fun, args)
+    expect_equal(x$parent, x$child, info = parent)
+    expect_equal(x$mutation_stats$n_mut, 0, info = parent)
+  }
 })
