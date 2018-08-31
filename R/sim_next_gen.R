3da541b7e9698a27fa0ee28fb11e28e381fae877
Phillip Labuschagne <philliplab@gmail.com>
2018-08-31 12:17:04 +0200

    MODIFY sim_next_gen to internally use the genealogy data.frame

--- R/sim_next_gen.R
+++ R/sim_next_gen.R
@@ -8,9 +8,22 @@
 #' @param gen_size The size of each generation. Currently only allowed to be a
 #' single integer. Default value is 2.
 #' @param mutator A list with two elements fun and args specifying the function that mutates parents into their offspring and the list of arguments said function requires. 
+#' @param gen_num The generation number. Defaults to 1.
 #' @export
 
-sim_next_gen <- function(parents, gen_size, mutator){
+sim_next_gen <- function(parents, gen_size, mutator, gen_num = 1){
+  genealogy <- data.frame(gen_num = numeric(0),
+                          id = numeric(0),
+                          parent_id = numeric(0),
+                          the_seq = character(0),
+                          n_mut = character(0),
+                          recomb_pos = numeric(0),
+                          recomb_replaced = character(0),
+                          recomb_partner = numeric(0),
+                          recomb_muts = numeric(0),
+                          fitness_score = numeric(0),
+                          stringsAsFactors = FALSE
+                          )
   offspring <- character(length(parents)*gen_size)
   total_offspring <- 0
   result <- list()
@@ -22,9 +35,21 @@ sim_next_gen <- function(parents, gen_size, mutator){
       mut_result <- do.call(mutator$fun, mut_arg)
       child <- mut_result$child
       offspring[total_offspring] <- child
+      c_genealogy <- data.frame(gen_num = gen_num,
+                                id = total_offspring,
+                                parent_id = i,
+                                the_seq = child,
+                                n_mut = mut_result$mutation_stats$n_mut,
+                                recomb_pos = NA_real_,
+                                recomb_replaced = NA_character_,
+                                recomb_partner = NA_real_,
+                                recomb_muts = NA_real_,
+                                fitness_score = NA_real_,
+                                stringsAsFactors = FALSE)
+      genealogy <- rbind(genealogy, c_genealogy, stringsAsFactors = FALSE)
     }
   }
-  result$offspring <- offspring
+  result$offspring <- genealogy$the_seq
   return(result)
 }
 
