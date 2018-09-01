a0219c196e00a81b24b0097b5cc80a2ed3f2f989
Phillip Labuschagne <philliplab@gmail.com>
2018-09-01 18:01:45 +0200

    Add basic functional tests for genealogies

--- tests/testthat/test_genealogy.R
+++ tests/testthat/test_genealogy.R
@@ -1,5 +1,30 @@
 context('genealogy')
 
+genealogy_expector <- function(genealogy){
+  y <- check_genealogy(genealogy)
+  for (i in names(y)){
+    expect_true(y[[i]], info = i)
+  }
+}
+
+test_that('check_genealogy works', {
+  ancestors <- 'AAA'
+  x <- data.frame(gen_num = 0,
+                  id = 1:length(ancestors),
+                  parent_id = -1,
+                  the_seq = ancestors,
+                  n_mut = NA_real_,
+                  recomb_pos = NA_real_,
+                  recomb_replaced = NA_character_,
+                  recomb_partner = NA_real_,
+                  recomb_muts = NA_real_,
+                  fitness_score = NA_real_,
+                  stringsAsFactors = FALSE
+                  )
+  genealogy_expector(x)
+})
+
 test_that('make_genealogy works', {
-  expect_equal(1+1,2)
+  x <- make_genealogy(ancestors = c('AAA'))
+  genealogy_expector(x)
 })
