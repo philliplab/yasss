9b1049f099e1fdbf1bd092354d68b1c83d4b0fa2
Phillip Labuschagne <philliplab@gmail.com>
2018-09-02 09:41:10 +0200

    ADD 2 generation sample genealogy for testing

--- tests/testthat/test_genealogy.R
+++ tests/testthat/test_genealogy.R
@@ -1,12 +1,31 @@
 context('genealogy')
 
-genealogy_expector <- function(genealogy, expect_false = NULL){
+SAMPLE_GENEALOGIES <- list(
+  bif_2gen = data.frame(
+#==========================================================
+gen_num          =  c(     0,      1,      1),
+id               =  c(     1,      1,      2),
+parent_id        =  c(     NA,     1,      1),
+the_seq          =  c(     'AAA',  'AAC',  'AAA'),
+n_mut            =  c(     NA,     1,      0),
+recomb_pos       =  c(     NA,     NA,     NA),
+recomb_replaced  =  c(     NA,     NA,     NA),
+recomb_partner   =  c(     NA,     NA,     NA),
+recomb_muts      =  c(     NA,     NA,     NA),
+fitness_score    =  c(     0.95,   0.99,   0.96),
+#==========================================================
+stringsAsFactors = FALSE
+  )
+                           
+                           )
+
+genealogy_expector <- function(genealogy, expect_false = NULL, extra_info = ''){
   y <- check_genealogy(genealogy)
   for (i in setdiff(names(y), expect_false)){
-    expect_true(y[[i]], info = i)
+    expect_true(y[[i]], info = paste(i, extra_info, sep = ' '))
   }
   for (i in expect_false){
-    expect_false(y[[i]], info = i)
+    expect_false(y[[i]], info = paste(i, extra_info, sep = ' '))
   }
 }
 
@@ -40,6 +59,11 @@ test_that('check_genealogy lets correct genealogies pass', {
                   stringsAsFactors = FALSE
                   )
   genealogy_expector(x)
+
+  for (c_name in names(SAMPLE_GENEALOGIES)){
+    c_genea <- SAMPLE_GENEALOGIES[[c_name]]
+    genealogy_expector(c_genea, extra_info = c_name)
+  }
 })
 
 test_that('check_genealogy flags issues with missing columns', {
@@ -83,6 +107,14 @@ test_that('check_genealogy flags issues with unexpected columns', {
     y[,i] <- 1
     genealogy_expector(y, expect_false = c('number_of_columns', 'column_order'))
   }
+})
+
+test_that('check_genealogy flags issues with gen_num', {
+  expect_equal(1+1,2)
+
+#  genealogy_expector(y, expect_false = c('gen_num may not be missing'))
+
+#  genealogy_expector(y, expect_false = c('gen_num must contain each integer between zero and max(gen_num)'))
 })
 
 test_that('make_genealogy works', {
