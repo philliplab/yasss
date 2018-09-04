30458ccd5f8bd6f7d707f41b0f0d68d10b20c737
Phillip Labuschagne <philliplab@gmail.com>
2018-09-04 17:52:32 +0200

    REORGANIZE the first fitness_evaluator and add first tests

---
+++ tests/testthat/test_fitness_evaluator_general.R
@@ -0,0 +1,10 @@
+context("fitness_evaluator_general")
+
+test_that("check_fitness_evaluator works", {
+  fe <- list(fun = 'fitness_evaluator_random',
+             args = NULL)
+  x <- check_fitness_evaluator(fe$fun, fe$args)
+  for (i in names(x)){
+    expect_true(x[[i]], info = i)
+  }
+})
