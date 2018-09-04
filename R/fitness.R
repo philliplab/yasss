3f2da05f231711d0a18ba50b0ed322a577f7d1ef
Phillip Labuschagne <philliplab@gmail.com>
2018-09-04 18:48:03 +0200

    Document assign_fitness

--- R/fitness.R
+++ R/fitness.R
@@ -1,6 +1,13 @@
+#' Assign fitness to sequences
+#'
+#' Given a fitness evaluator, assign all the sequences in the last generation of a genealogy a fitness
+#'
+#' @return A genealogy
+#' @param genealogy A genealogy
+#' @param fitness_evaluator A list with two elements: fun is the character string naming a fitness evaluator and args is a list of arguments that fun needs.
+#' @export
+
 assign_fitness <- function(genealogy, fitness_evaluator){
-
-
   args <- fitness_evaluator$args
   args$the_seq <- genealogy$the_seq
   fit_fun <- get(fitness_evaluator$fun)
@@ -9,16 +16,6 @@ assign_fitness <- function(genealogy, fitness_evaluator){
   stopifnot(all(x$the_seq == genealogy$the_seq))
   genealogy$fitness <- x$fitness
 
-
-
-
-#  for (i in 1:nrow(genealogy)){
-#    args <- fitness_evaluator$args
-#    args$the_seq <- genealogy[i,'the_seq']
-#    fit_fun <- get(fitness_evaluator$fun)
-#    x <- do.call(fit_fun, args)
-#    genealogy[i,'fitness'] <- x$fitness
-#  }
   return(genealogy)
 }
 
