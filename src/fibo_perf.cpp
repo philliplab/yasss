a612d1bc3ff9f7516f07a4ce076d25efef99b377
Phillip Labuschagne <philliplab@gmail.com>
2018-10-02 13:17:53 +0200

    ADD first version of cpp_fibo_df

--- src/fibo_perf.cpp
+++ src/fibo_perf.cpp
@@ -4,7 +4,6 @@ using namespace Rcpp;
 // [[Rcpp::plugins(cpp14)]]                                        
 
 // [[Rcpp::export]]
-
 NumericVector cpp_fibo(double s1, double s2, int n) {
 
   std::vector<double> x = {s1, s2};
@@ -15,5 +14,33 @@ NumericVector cpp_fibo(double s1, double s2, int n) {
   return wrap(x);
 }
 
+std::vector<double> cpp_fibo_no_wrap(double s1, double s2, int n) {
+
+  std::vector<double> x = {s1, s2};
+
+  for (int i = 2; i < n; i++){
+    x.push_back(pow(x[i-2], 0.25) + x[i-1]);
+  }
+  return x;
+}
+
+
+// [[Rcpp::export]]
+List cpp_fibo_df(int n, int n_columns) {
+  std::vector<std::vector<double>> x;
+//  std::vector<double> y;
+
+
+  double s1 = 1;
+  double s2 = 1;
+  for (int i = 0; i < n_columns; i++){
+//    y = cpp_fibo_no_wrap(s1, s2, n);
+//    y.push_back(1);
+//    y.push_back(1);
+//    y.push_back(1);
+    x.push_back(cpp_fibo_no_wrap(s1, s2, n));
+  }
+  return wrap(x);
+}
 
 
