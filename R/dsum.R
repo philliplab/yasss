5dd605b1e90da3f050ad58df59d9c33710dceff3
Phillip Labuschagne <philliplab@gmail.com>
2018-10-18 11:11:23 +0200

    ADD skeletons for check_dsum

---
+++ R/dsum.R
@@ -0,0 +1,15 @@
+#' Check a dsum list
+#'
+#' Checks that a dsum list adheres to the specifications
+#'
+#' @return A list with TRUE or FALSE indicating whether the related check
+#' passed.
+#' @param dsum The dsum to check
+#' previous result will be drawn to check the prerequisites.
+#' @export
+
+check_dsum <- function(dsum){
+  result <- list()
+  return(result)
+}
+    
---
+++ tests/testthat/test_dsum.R
@@ -0,0 +1,5 @@
+context('dsum')
+
+test_that('check_dsum works', {
+  expect_equal(1+1, 2)
+})
