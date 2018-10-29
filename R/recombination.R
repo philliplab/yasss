ae86855ffcdb6f2e4cbbd407ac90310b84ed9726
Phillip Labuschagne <philliplab@gmail.com>
2018-10-29 20:48:46 +0200

    ADD functionality to recombine_seqs and first tests

--- R/recombination.R
+++ R/recombination.R
@@ -12,6 +12,28 @@ recombine_gen <- function(gen){
 #' @param recombination_partner_seq The recombination partner sequence
 #' @export
 
-recombine_seqs <- function(target_seq, recombination_partner_seq){
-  return(list())
+recombine_seqs <- function(target_seq, recombination_partner_seq, min_dist_to_edge = 5){
+  stopifnot(length(target_seq) == 1)
+  stopifnot(length(recombination_partner_seq) == 1)
+  stopifnot(nchar(target_seq) == nchar(recombination_partner_seq))
+  stopifnot(min_dist_to_edge < nchar(target_seq)/2.2)
+  stopifnot(min_dist_to_edge >= 0 & min_dist_to_edge <= nchar(target_seq))
+
+  breakpoint <- runif(1, min_dist_to_edge, nchar(target_seq) - min_dist_to_edge)
+  target_on_LHS <- ifelse(runif(1) > 0.5, TRUE, FALSE)
+
+  if (target_on_LHS){
+    recombinant_LHS <- substr(target_seq, 1, breakpoint)
+    recombinant_RHS <- substr(recombination_partner_seq, breakpoint + 1, nchar(target_seq))
+  } else {
+    recombinant_LHS <- substr(recombination_partner_seq, 1, breakpoint)
+    recombinant_RHS <- substr(target_seq, breakpoint + 1, nchar(target_seq))
+  }
+
+  recombinant <- paste(recombinant_LHS, recombinant_RHS, sep = '')
+  dist_to_target_seq <- stringdist(recombinant, target_seq, method = 'hamming')
+
+  return(list(recombinant = recombinant,
+              dist_to_target_seq = dist_to_target_seq,
+              target_on_LHS = target_on_LHS))
 }
--- tests/testthat/test_recombination.R
+++ tests/testthat/test_recombination.R
@@ -7,6 +7,22 @@ test_that('recombine_gen works', {
 
 context('recombine_seqs')
 
+target_seq <- 'AAAAAAAAAA'
+recombination_partner_seq <- 'CCCCCCCCCC'
+min_dist_to_edge <- 2
+
 test_that('recombine_seqs works', {
   expect_equal(1+1, 2)
+  x <- recombine_seqs(target_seq, recombination_partner_seq, 2)
+  first_char <- substr(x$recombinant, 1, 1)
+  last_char <- substr(x$recombinant, 10, 10)
+  expect_equal(length(x), 3)
+  if (x$target_on_LHS){
+    expect_equal(first_char, 'A')
+    expect_equal(last_char, 'C')
+  } else {
+    expect_equal(first_char, 'C')
+    expect_equal(last_char, 'A')
+  }
+
 })
