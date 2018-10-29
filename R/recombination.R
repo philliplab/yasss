31aec5e662ceb69a00fd93b2e18301aade07ad11
Phillip Labuschagne <philliplab@gmail.com>
2018-10-29 09:55:54 +0200

    ADD recombination function skeletons

---
+++ R/recombination.R
@@ -0,0 +1,17 @@
+#' Recombines sequences in a generation
+#' @param gen A data.frame from a genealogy that contains just one
+#' generation
+#' @export
+
+recombine_gen <- function(gen){
+  return(gen)
+}
+
+#' Recombines two sequences
+#' @param target_seq The sequence that will be replaced by the recombination
+#' @param recombination_partner_seq The recombination partner sequence
+#' @export
+
+recombine_seqs <- function(target_seq, recombination_partner_seq){
+  return(list())
+}
---
+++ tests/testthat/test_recombination.R
@@ -0,0 +1,12 @@
+context('recombine_gen')
+
+test_that('recombine_gen works', {
+  expect_equal(1+1, 2)
+})
+
+
+context('recombine_seqs')
+
+test_that('recombine_seqs works', {
+  expect_equal(1+1, 2)
+})
