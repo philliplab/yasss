99f22a130049edf7c04195a694dd760175f3fc69
Phillip Labuschagne <philliplab@gmail.com>
2018-10-30 19:26:10 +0200

    ADD testing tools for recombine_gen

--- tests/testthat/test_recombination.R
+++ tests/testthat/test_recombination.R
@@ -1,7 +1,26 @@
 context('recombine_gen')
 
+genea <- sim_pop(ancestors = paste(rep("A", 60), collapse = ''),
+                 r0 = 2,
+                 n_gen = 5,
+                 n_pop = Inf,
+                 mutator = list(fun = "mutator_uniform_fun",
+                                args = list(mu = 0.1)),
+                 fitness_evaluator = list(fun = "fitness_evaluator_uniform_fun",
+                                           args = NULL))
+
+last_gen <- genea %>% filter(gen_num == max(gen_num))
+
+genealogy_cols <-
+  c("gen_num", "id", "parent_id", "the_seq", "n_mut", "recomb_pos",
+    "recomb_replaced", "recomb_partner", "recomb_muts", "fitness_score")
+
 test_that('recombine_gen works', {
-  expect_equal(1+1, 2)
+  x <- recombine_gen(last_gen)
+  expect_true(class(x) == 'data.frame')
+  for (i in genealogy_cols){
+    expect_true(i %in% names(x), info = paste("Column ", i, " in output", sep = ''))
+  }
 })
 
 
@@ -12,7 +31,7 @@ recombination_partner_seq <- 'CCCCCCCCCC'
 min_dist_to_edge <- 2
 
 test_that('recombine_seqs works', {
-  x <- recombine_seqs(target_seq, recombination_partner_seq, 2)
+  x <- recombine_seqs(target_seq, recombination_partner_seq, min_dist_to_edge)
   expect_equal(class(x), 'list')
   expect_equal(length(x), 4)
   expect_true('recombinant' %in% names(x))
@@ -40,5 +59,7 @@ test_that('recombine_seqs works', {
   last_lhs <- substr(x$recombinant, x$recomb_pos, x$recomb_pos)
   first_rhs <- substr(x$recombinant, x$recomb_pos+1, x$recomb_pos+1)
   expect_false(last_lhs == first_rhs)
+  expect_true(x$recomb_pos >= min_dist_to_edge)
+  expect_true(x$recomb_pos <= nchar(target_seq) - min_dist_to_edge)
 
 })
