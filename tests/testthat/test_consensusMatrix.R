361254b883e8fc914377a465b76572ce2d691349
Phillip Labuschagne <philliplab@gmail.com>
2018-11-02 11:17:41 +0200

    ADD consensusMatrix test to ensure dimensions handled in correct order

--- tests/testthat/test_consensusMatrix.R
+++ tests/testthat/test_consensusMatrix.R
@@ -1,7 +1,7 @@
 context("consensusMatrix")
 
-x <- sample(c('A','C','G','T'), 100, replace = TRUE)
-x <- matrix(x, ncol = 10, nrow = 10)
+x <- sample(c('A','C','G','T'), 50, replace = TRUE)
+x <- matrix(x, ncol = 5, nrow = 10)
 seqs <- apply(x, 1, paste, sep = '', collapse = '')
 
 test_that("consensusMatrix works", {
@@ -9,8 +9,11 @@ test_that("consensusMatrix works", {
   the_rownames <- c("A", "C", "G", "T", "M", "R", "W", "S", "Y", "K", "V", "H",
                     "D", "B", "N", "-", "+", ".")
   expect_equal(attr(cm, "dimnames")[[1]], the_rownames)
-  expect_equal(unique(apply(cm, 2, sum)), 10)
-  expect_equal(dim(cm), c(18, 10))
+
+  expect_equal(min(apply(cm, 2, sum)), min(apply(cm, 2, sum)))
+  
+  expect_equal(unique(apply(cm, 2, sum)), 5)
+  expect_equal(dim(cm), c(18, 5))
 })
 
 
